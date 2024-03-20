3c6dbab81c2af7f1c6c90ba697ec8cddaad0ef5f
Proud Lobster <proud.lobster.games@gmail.com>
2024-03-19 21:09:49 -0400

    Singleton endpoint proto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,34 @@ import org.eclipse.jetty.server.Server;
 
 import com.proudlobster.stacks.Stacks;
 
+import jakarta.websocket.server.ServerEndpointConfig;
+
 public interface Main {
     public static void main(String[] args) throws Exception {
-        Stacks.create();
+        final Stacks stacks = Stacks.create();
         System.out.println("Stacks!!!");
 
         Server s = new Server(8080);
-        ServletContextHandler handler = new ServletContextHandler("/ctx");
+        ServletContextHandler handler = new ServletContextHandler("/");
         s.setHandler(handler);
 
+        PrototypeEndpoint endpoint = new PrototypeEndpoint();
+
+        ServerEndpointConfig.Configurator conf = new ServerEndpointConfig.Configurator() {
+            @Override
+            public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
+                if (endpointClass.isInstance(endpoint)) {
+                    return endpointClass.cast(endpoint);
+                } else {
+                    throw new InstantiationException("No endpoint for type " + endpointClass.getName());
+                }
+            }
+        };
+        ServerEndpointConfig endpointConfig = ServerEndpointConfig.Builder.create(PrototypeEndpoint.class, "/test")
+                .configurator(conf)
+                .build();
         JakartaWebSocketServletContainerInitializer.configure(handler,
-                (ctx, srv) -> srv.addEndpoint(PrototypeEndpoint.class));
+                (ctx, srv) -> srv.addEndpoint(endpointConfig));
 
         s.start();
         System.out.println("Server running.");
--- src/main/java/PrototypeEndpoint.java
+++ src/main/java/PrototypeEndpoint.java
@@ -1,12 +1,27 @@
-import jakarta.websocket.OnMessage;
-import jakarta.websocket.server.ServerEndpoint;
+import java.io.IOException;
 
-@ServerEndpoint("/test")
-public class PrototypeEndpoint {
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.EndpointConfig;
+import jakarta.websocket.MessageHandler;
+import jakarta.websocket.Session;
 
-    @OnMessage
-    public String handleMessage(String message) {
-        return "Echo: " + message;
+public class PrototypeEndpoint extends Endpoint {
+
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
+        session.addMessageHandler(new MessageHandler.Whole<String>() {
+
+            @Override
+            public void onMessage(String message) {
+                try {
+                    System.out.println(session.getId() + " : " + message);
+                    session.getBasicRemote().sendText("Echo: " + message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
     }
 
 }
